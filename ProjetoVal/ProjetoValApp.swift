fa3214785ccc3dbfcbd66da9ce31919313844a9e
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2023-11-08 10:30:41 -0300

    Initial Commit

---
+++ ProjetoVal/ProjetoValApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoValApp.swift
+//  ProjetoVal
+//
+//  Created by Laura C. Balbachan dos Santos on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoValApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
